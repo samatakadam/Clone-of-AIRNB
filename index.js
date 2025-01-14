6df1a43a0eb6793318e15dd3ee0f3486904b4c6f
Samata Kadam <166407004+samatakadam@users.noreply.github.com>
2025-01-14 20:14:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "remixicon/fonts/remixicon.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { BrowserRouter } from "react-router-dom";
-import { AuthContextProvider } from "./context/AuthContext";
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <AuthContextProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    </AuthContextProvider>
-  </React.StrictMode>
-);
+import tourRoute from './routes/tours.js'
+import userRoute from './routes/users.js'
+import authRoute from './routes/auth.js'
+import reviewRoute from './routes/reviews.js'
+import bookingRoute from './routes/bookings.js'
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 8000
+
+const corsOptions ={
+    origin:'http://localhost:3000',
+    credentials:true
+}
+
+//db connectivity
+const connect = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://samatakk23:O1C2MlensFqHppXa@cluster0.olnha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        console.log('MongoDB database connected');
+    } catch (err) {
+        console.error('MongoDB connection failed:', err.message);
+    }
+};
+
+// middleware
+app.use(express.json())
+app.use(cors(corsOptions))
+app.use(cookieParser())
+
+app.use('/api/v1/auth',authRoute)
+app.use('/api/v1/tours',tourRoute)
+app.use('/api/v1/users',userRoute)
+app.use('/api/v1/review',reviewRoute)
+app.use('/api/v1/book',bookingRoute)
+
+app.listen(port ,()=>{
+    connect()
+    console.log(`server is running on port ${port}`)
+})
